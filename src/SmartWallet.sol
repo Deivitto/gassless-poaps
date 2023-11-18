bbfc972b5e5fa300912271b32d3502cb08b3bb61
pm <misirov@pablos-MacBook-Air.local>
2023-11-19 00:15:11 +0300

    cleanup

--- src/SmartWallet.sol
+++ src/SmartWallet.sol
@@ -15,45 +15,9 @@ contract SmartWallet is BaseAccount{
         owner = _owner;
     }
 
-    // this should be implemented in the front end via an SDK!
-    function createUserOp(
-        address _sender,               // sender of the user op
-        uint256 _nonce,                // nonce for replay protection
-        bytes memory _initCode,         // if set, for contract creation
-        bytes memory _callData,         // methods to execute on THIS contract
-        uint256 _callGasLimit,          // gas limit passed to callData method call
-        uint256 _verificationGasLimit,  // gas for validateUserOps and validatePaymasterUserOp
-        uint256 _preVerificationGas,    // gas to cover batch overhead
-        uint256 _maxFeePerGas,          // same as EIP-1559 gas parameter
-        uint256 _maxPriorityFeePerGas,  // same as EIP-1559 gas parameter
-        bytes memory _paymasterAndData, // If set, this field holds the paymaster address and paymaster-specific data.
-        bytes memory _signature         // Sender-verified signature over the entire request, the EntryPoint address and the chain ID
-        ) external pure returns (UserOperation memory) {
-
-            UserOperation memory userOp = UserOperation({
-                sender: _sender,
-                nonce: _nonce,
-                initCode: _initCode,
-                callData: _callData,
-                callGasLimit: _callGasLimit,
-                verificationGasLimit: _verificationGasLimit,
-                preVerificationGas: _preVerificationGas,
-                maxFeePerGas: _maxFeePerGas,
-                maxPriorityFeePerGas: _maxPriorityFeePerGas,
-                paymasterAndData: _paymasterAndData,
-                signature: _signature
-            });
-
-            return userOp;
-
-    }
-
-
     function entryPoint() public view override returns (IEntryPoint) {
         return entryPointContract;
     }
-
-
 
 
     function hello() public view returns(string memory){
