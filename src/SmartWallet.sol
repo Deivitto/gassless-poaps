1be56d0781d23b41b1ed1ef344d332ea4cb126c3
pm <misirov@pablos-MacBook-Air.local>
2023-11-18 11:21:25 +0300

    base

---
+++ src/SmartWallet.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+
+
+interface IEntryPoint {
+
+struct UserOps {
+
+}
+
+    
+}
+
+contract SmartWallet {
+
+
+    constructor(){}
+
+
+}
---
+++ test/SmartWallet.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {SmartWallet} from "../src/SmartWallet.sol";
+
+contract CounterTest is Test {
+
+    function setUp() public {
+
+    }
+
+}
